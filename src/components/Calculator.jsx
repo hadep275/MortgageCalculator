d0aca98d9ca4c50c1c450de0ea234ba95c45d2e4
hadep275 <hadep275@gmail.com>
2023-11-14 18:56:28 -0700

    content updated

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,15 @@
+// Calculator.js
+import React from 'react'
+import axios from 'axios';
+import '../styles/Calculator.css';
+
+const Calculator = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Calculator
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+// Header.js
+import React from 'react';
+import '../styles/Header.css';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Mortgage Calculator</h1>
+      {/* Add any other content for the header here */}
+    </header>
+  );
+};
+
+export default Header;
