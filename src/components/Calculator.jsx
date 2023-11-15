5f248560c77c8a37a321e22ae3aa77debf85961b
hadep275 <hadep275@gmail.com>
2023-11-14 19:37:23 -0700

    calculator css updated.

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,15 +1,73 @@
 // Calculator.js
-import React from 'react'
-import axios from 'axios';
+import React, {useState} from 'react'
 import '../styles/Calculator.css';
 
 const Calculator = () => {
+  const [loanAmount, setLoanAmount] = useState('');
+  const [interestRate, setInterestRate] = useState('');
+  const [loanTerm, setLoanTerm] = useState('');
+  const [propertyTaxes, setPropertyTaxes] = useState('');
+  const [monthlyPayment, setMonthlyPayment] = useState(null);
+
+  const calculateMortgage = () => {
+    const principal = parseFloat(loanAmount);
+    const monthlyRate = (parseFloat(interestRate) / 100) / 12;
+    const numberOfPayments = parseFloat(loanTerm) * 12;
+    const taxes = parseFloat(propertyTaxes);
+
+
+    const monthlyPaymentResult =
+      ((principal + taxes) * monthlyRate) /
+      (1 - Math.pow(1 + monthlyRate, -numberOfPayments));
+
+    setMonthlyPayment(monthlyPaymentResult.toFixed(2));
+  };
+
   return (
-    <div>
-      
+    <div className="calculator">
+    <h2>Mortgage Calculator</h2>
+    <div className="input-group">
+      <label>Loan Amount ($)</label>
+      <input
+        type="number"
+        value={loanAmount}
+        onChange={(e) => setLoanAmount(e.target.value)}
+      />
     </div>
-  )
-}
+    <div className="input-group">
+      <label>Interest Rate (%)</label>
+      <input
+        type="number"
+        value={interestRate}
+        onChange={(e) => setInterestRate(e.target.value)}
+      />
+    </div>
+    <div className="input-group">
+      <label>Loan Term (Years)</label>
+      <input
+        type="number"
+        value={loanTerm}
+        onChange={(e) => setLoanTerm(e.target.value)}
+      />
+    </div>
+    <div className="input-group">
+        <label>Property Taxes ($)</label>
+        <input
+          type="number"
+          value={propertyTaxes}
+          onChange={(e) => setPropertyTaxes(e.target.value)}
+        />
+      </div>
+    <button onClick={calculateMortgage}>Calculate</button>
+    {monthlyPayment !== null && (
+      <div className="results">
+        <h3>Monthly Payment</h3>
+        <span>${monthlyPayment}</span>
+      </div>
+    )}
+  </div>
+);
+};
 
 export default Calculator
 
